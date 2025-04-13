9161dd52a004bab8dde2b6b5ba08f538440ee43b
Ajay Tiwari <133643890+012Ajaytiwari@users.noreply.github.com>
2025-04-14 01:09:26 +0530

    Update UserLoginDto.cs

--- UserLoginDto.cs
+++ UserLoginDto.cs
@@ -1,1 +1,16 @@
-// DTO for user login
+// DTOusing System.ComponentModel.DataAnnotations;
+
+namespace MiniBlogAPI.ViewModels
+{
+    public class LoginDto
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
+ for user login
