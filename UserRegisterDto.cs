cc76610e72c412038fba0e1e95cd683a54f7ed78
Ajay Tiwari <133643890+012Ajaytiwari@users.noreply.github.com>
2025-04-14 01:10:40 +0530

    Update UserRegisterDto.cs

--- UserRegisterDto.cs
+++ UserRegisterDto.cs
@@ -1,1 +1,22 @@
-// DTO for user registration
+//using System.ComponentModel.DataAnnotations;
+
+namespace MiniBlogAPI.ViewModels
+{
+    public class RegisterDto
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
+ DTO for user registration
