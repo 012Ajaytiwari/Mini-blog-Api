7b6776ab107f4d0c5430cf1651b6fb5d76bf4e97
Ajay Tiwari <133643890+012Ajaytiwari@users.noreply.github.com>
2025-04-14 00:54:27 +0530

    Update ApplicationDbContext.cs

--- ApplicationDbContext.cs
+++ ApplicationDbContext.cs
@@ -1,1 +1,34 @@
-// EF Core DB context
+// EFusing Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MiniBlogAPI.Models;
+
+namespace MiniBlogAPI.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Additional model configuration here
+            modelBuilder.Entity<BlogPost>()
+                .HasOne(p => p.Author)
+                .WithMany(u => u.BlogPosts)
+                .HasForeignKey(p => p.AuthorId);
+
+            modelBuilder.Entity<Comment>()
+                .HasOne(c => c.BlogPost)
+                .WithMany(p => p.Comments)
+                .HasForeignKey(c => c.BlogPostId);
+        }
+    }
+}
+ Core DB context
